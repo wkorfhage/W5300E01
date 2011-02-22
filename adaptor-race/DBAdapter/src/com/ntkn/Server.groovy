26396fcdb1c1be72d30d967408444133c02235f1
lqu <quliang8247@gmail.com>
2011-02-22 14:42:08 -0600

    the real one that logs the client socket.

--- adaptor-race/DBAdapter/src/com/ntkn/Server.groovy
+++ adaptor-race/DBAdapter/src/com/ntkn/Server.groovy
@@ -1,14 +1,14 @@
-
 package com.ntkn
+
 import groovy.sql.Sql;
 import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 
-def log = GroovyLog.netInstance(Server.class)
+def log = GroovyLog.newInstance(Server.class)
 
 def dataSource = new MysqlConnectionPoolDataSource();
 dataSource.setUser("lqu")
 dataSource.setPassword("lqu")
-dataSource.setURL("jdbc:mysql://localhost/test")
+dataSource.setURL("jdbc:mysql://localhost/adapter")
 
 //String createDB = "CREATE TABLE RECORD (id BIGINT NOT NULL auto_increment, counter INT NOT NULL, delta INT NOT NULL, name VARCHAR(255) NOT NULL, rank INT NOT NULL, time VARCHAR(255) NOT NULL, PRIMARY KEY (id)) ENGINE=MyISAM;";
 
@@ -21,9 +21,10 @@ while(true) {
 	DatagramPacket incoming = new DatagramPacket(buffer, buffer.length)
 	socket.receive(incoming)
 
-	
 	String s = new String(incoming.data, 0, incoming.length)
-
+	log.client("" + incoming.getAddress() + ":" + incoming.getPort())
+	log.content(s);
+	
 	if (s.startsWith("Tell me the time")) {
 		println new Date()
 		byte[] b = new java.sql.Timestamp(new java.util.Date().getTime()).toString().getBytes()
@@ -32,16 +33,15 @@ while(true) {
 	} else {
 		s = s.replaceAll("\n", "")
 		s = s.replaceAll("\000", "")
-		String reply = "Client said: '$s'"
-		println reply
+
 		sa = s.split(",")
-		println sa
 		
 		//def sql = Sql.newInstance("jdbc:mysql://localhost/test", "lqu", "lqu", "com.mysql.jdbc.Driver");
 		//sql.execute("insert into stat values ('2010-12-12 22:22:22.222', 'name', 12345, 0, 1)")
 		
 		def sql = new Sql(dataSource)
-		sql.execute("insert into ADAPTER (id, time, name, counter, delta, rank) values (null, ${sa[0]}, ${sa[1]}, ${sa[2]}, ${sa[3]}, ${sa[4]})")
+		sql.execute("insert into adapter (id, time, name, counter, delta, rank) values (null, ${sa[0]}, ${sa[1]}, ${sa[2]}, ${sa[3]}, ${sa[4]})")
 		sql.close()
 	}
 }
+
